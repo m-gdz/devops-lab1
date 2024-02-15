a4739890ab47e8cd16c3c92ca1add9086975008d
m-gdz <34210196+m-gdz@users.noreply.github.com>
2024-02-15 12:33:10 +0100

    update grade record

--- build/lib/swagger_server/models/student.py
+++ build/lib/swagger_server/models/student.py
@@ -38,7 +38,7 @@ class Student(Model):
             'student_id': 'student_id',
             'first_name': 'first_name',
             'last_name': 'last_name',
-            'grad_records': 'gradRecords'
+            'grade_records': 'gradeRecords'
         }
         self._student_id = student_id
         self._first_name = first_name
